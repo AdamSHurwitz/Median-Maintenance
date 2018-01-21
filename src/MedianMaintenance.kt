6a0080db628abcd7ed20dd36a8f5bcd3184f967f
Adam Hurwitz <adamscotthurwitz@gmail.com>
2018-01-20 22:22:30 -0800

    solved

--- src/MedianMaintenance.kt
+++ src/MedianMaintenance.kt
@@ -1,3 +1,38 @@
-class MedianMaintenance{
+import java.util.*
 
+class MedianMaintenance {
+    var lowHeap = PriorityQueue<Int>(Collections.reverseOrder())
+    var highHeap = PriorityQueue<Int>()
+    var medianSum = 0
+
+    fun compute(array: IntArray): Int {
+        for (i in 1..array.size) {
+            if (lowHeap.isEmpty() || array[i - 1] < lowHeap.peek()) {
+                lowHeap.add(array[i - 1])
+            } else {
+                highHeap.add(array[i - 1])
+            }
+            if (Math.abs(lowHeap.size - highHeap.size) > 1) {
+                if (Math.max(lowHeap.size, highHeap.size) == lowHeap.size) {
+                    highHeap.add(lowHeap.poll())
+                } else {
+                    lowHeap.add(highHeap.poll())
+                }
+            }
+            getMedianSum(i)
+        }
+        return medianSum;
+    }
+
+    private fun getMedianSum(i: Int) {
+        if (i % 2 == 0) {
+            medianSum += lowHeap.peek()
+        } else {
+            if (Math.max(lowHeap.size, highHeap.size) == lowHeap.size) {
+                medianSum += lowHeap.peek()
+            } else {
+                medianSum += highHeap.peek()
+            }
+        }
+    }
 }
--- src/MedianMaintenanceTest.kt
+++ src/MedianMaintenanceTest.kt
@@ -1,5 +1,17 @@
+import java.io.File
+
 object MedianMaintenanceTest {
     @JvmStatic
-    fun main(args: Array<String>){
+    fun main(args: Array<String>) {
+        var list = arrayListOf<Int>()
+        File("src/data/fullDataSet.txt")
+                .inputStream()
+                .bufferedReader()
+                .useLines { lines ->
+                    lines.forEach {
+                        list.add(it.toInt())
+                    }
+                }
+        println("Medium Sum: " + MedianMaintenance().compute(list.toIntArray()))
     }
 }
